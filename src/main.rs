2e549cb5b17b9a60f0607c6603ef38c79498611c
Dorian Moy <dorian.moy@proton.me>
2026-05-29 23:45:01 +0200

    refactor(lib): extracted lib crate exposing app config for tests

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,7 @@
-mod controller;
-mod services;
-
-use std::sync::LazyLock;
-
 use actix_web::{App, HttpServer, web::Data};
-use regex::Regex;
-use serde::{Deserialize, Serialize};
 use sqlx::SqlitePool;
-use validator::Validate;
 
-use crate::{
-    controller::{redirect_to_url_for_id, shorten_url},
-    services::url_shortener::UrlShortenerService,
-};
-
-static RE_HTTP_SCHEME: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^https?://.+").unwrap());
-
-pub type DatabasePool = SqlitePool;
+use shorten_rs::{configure, services::url_shortener::UrlShortenerService};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -25,16 +10,9 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(url_shortener_service.clone()))
-            .service(shorten_url)
-            .service(redirect_to_url_for_id)
+            .configure(configure)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
-}
-
-#[derive(Debug, Deserialize, Serialize, Clone, Validate)]
-pub struct ShortenUrlDto {
-    #[validate(url, regex(path = *RE_HTTP_SCHEME))]
-    pub(crate) url: String,
 }
